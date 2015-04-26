24c0690222fa55e6b3de1a0e660940a334b0e75e
Geoff Moore <Geoffrey.Moore@sigmacubed.com.au>
2015-04-26 15:06:06 +1000

    Update Assignment 2 Ex.R

--- Assignment 2 Ex.R
+++ Assignment 2 Ex.R
@@ -1,4 +1,4 @@
-# Matrix inversion is usually a costly computation and there may be some benefit Hello World I want to get off
+# Matrix inversion is usually a costly computation and there may be some benefit
 # to caching the inverse of a matrix rather than compute it repeatedly. The
 # following two functions are used to cache the inverse of a matrix.
 
