315f66234f8dc35ba3bb1c39cab2b6efc09260a6
Geoff Moore <Geoffrey.Moore@sigmacubed.com.au>
2015-04-26 14:58:05 +1000

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,13 @@
-## Put comments here that give an overall description of what your
-## functions do
+
+# Matrix inversion is usually a costly computation and there may be some benefit Hello World I want to get off
+# to caching the inverse of a matrix rather than compute it repeatedly. The
+# following two functions are used to cache the inverse of a matrix.
+
+# makeCacheMatrix creates a list containing a function to
+# 1. set the value of the matrix
+# 2. get the value of the matrix
+# 3. set the value of inverse of the matrix
+# 4. get the value of inverse of the matrix
 
 ## Write a short comment describing this function
 
