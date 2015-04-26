fda9c1af98cf6be47ab81d25e1cb2b75764846bc
Geoff Moore <Geoffrey.Moore@sigmacubed.com.au>
2015-04-26 15:07:12 +1000

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,7 @@
 # following two functions are used to cache the inverse of a matrix.
 
 # makeCacheMatrix creates a list containing a function to
+
 # 1. set the value of the matrix
 # 2. get the value of the matrix
 # 3. set the value of inverse of the matrix
@@ -27,7 +28,7 @@ makeCacheMatrix <- function(x = matrix()) {
 # computation. If not, it computes the inverse, sets the value in the cache via
 # setinverse function.
 
-# This function assumes that the matrix is always invertible.
+# Note: This function assumes that the matrix is always invertible.
 
 cacheSolve <- function(x, ...) {
 
